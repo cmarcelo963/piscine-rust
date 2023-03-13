d5b0ead736468f136d3bb12676c5b0469e2c5938
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-13 15:17:03 +0000

    Looping

---
+++ looping/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut number_of_tries = 0u32;
+    
+    loop {
+        let mut input = String::new();
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+        io::stdin().read_line(&mut input).expect("failed to read line");
+        number_of_tries += 1;
+        if input.trim() == "e" {
+            println!("Number of trials: {}", number_of_tries);
+            break;
+        } 
+        println!("{}", input.trim());
+    }
+}
