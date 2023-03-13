f537bedf5e477fd679290fe3bf2a493d4ee405dd
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-13 11:54:37 +0000

    Change name

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn add(a: i32, b: i32) -> i32 {
+pub fn sum(a: i32, b: i32) -> i32 {
     a + b
 }
 pub fn diff(a: i32, b: i32) -> i32 {
@@ -7,10 +7,10 @@ pub fn diff(a: i32, b: i32) -> i32 {
 pub fn pro(a: i32, b: i32) -> i32 {
     a * b
 }
-pub fn quo(a: i32, b: i32) -> i32 {
+pub fn quo(a: f32, b: f32) -> f32 {
     a / b
 }
-pub fn rem(a: i32, b: i32) -> i32 {
+pub fn rem(a: f32, b: f32) -> f32 {
     a % b
 }
 #[cfg(test)]
@@ -19,15 +19,15 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = sum(2, 2);
         assert_eq!(result, 4);
         let result2 = diff(-32768, 32767);
         assert_eq!(result2, -65535);
         let result3 = pro(-128, 127);
         assert_eq!(result3, -16256);
-        let result4 = quo(59, 6);
-        assert_eq!(result4, 9);
-        let result5 = rem(59, 6);
-        assert_eq!(result5, 5);
+        let result4 = quo(59.0, 6.0);
+        assert_eq!(result4, 9.833333);
+        let result5 = rem(59.0, 6.0);
+        assert_eq!(result5, 5.0);
     }
 }
