52d01fe6df3484f88ce920a1e4726f579fb13092
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-13 15:19:22 +0000

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -8,10 +8,9 @@ fn main() {
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         io::stdin().read_line(&mut input).expect("failed to read line");
         number_of_tries += 1;
-        if input.trim() == "e" {
+        if input.trim() == "The letter e" {
             println!("Number of trials: {}", number_of_tries);
             break;
         } 
-        println!("{}", input.trim());
     }
 }
