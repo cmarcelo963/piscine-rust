a6f13da9298261d1a6cdc2f3f2a6135c866e4017
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-13 13:05:48 +0000

    Temperature converter

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,7 @@
-pub fn farenheit_to_celsius(f: f64) -> f64 {
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
     (f-32.0) * (5.0/9.0)
 }
-pub fn celsius_to_farenheit(c: f64) -> f64 {
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     (c*(9.0/5.0)) + 32.0
 }
 #[cfg(test)]
@@ -10,9 +10,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result: f64 = farenheit_to_celsius(-459.67);
+        let result: f64 = fahrenheit_to_celsius(-459.67);
         assert_eq!(result, -273.15000000000003);
-        let result2: f64 = celsius_to_farenheit(0.0);
+        let result2: f64 = celsius_to_fahrenheit(0.0);
         assert_eq!(result2, 32.0);
     }
 }
