f88899ff7c2b039b416a8059d35001ab6a293bc2
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-14 11:57:10 +0000

    Tuples refs

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct Student(u32, String, String);
+
+pub fn id(student: &Student) -> u32 {
+    let result = student.0;
+    return result
+}
+
+pub fn first_name(student: &Student) -> String {
+    let result = student.1.clone();
+    return result
+}
+
+pub fn last_name(student: &Student) -> String {
+    let result = student.2.clone();
+    return result
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
+        let result = (id(&student), first_name(&student),last_name(&student));
+        assert_eq!(result, (20, "Pedro".to_string(), "Domingos".to_string()));
+    }
+}
