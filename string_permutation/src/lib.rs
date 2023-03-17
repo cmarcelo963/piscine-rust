5c9cee3b640c4194e577db28985b95441c3ab7ed
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-17 18:05:19 +0000

    String permutation

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() {
+        return false;
+    }
     let s1chars = s1.chars();
     let s2chars = s2.chars();
     let mut s1char_map: HashMap<char, i32> = HashMap::new();
@@ -11,13 +14,21 @@ pub fn is_permutation(s1: &str, s2: &str) -> bool {
     for c in s2chars {
         *s2char_map.entry(c).or_insert(0) += 1;
     }
-    let mut all_equal = true;
+    let mut first_equal = true;
     for (key, value1) in &s1char_map {
         match s2char_map.get(key) {
             Some(value2) if value1 == value2 => {},
-            _ => { all_equal = false; break; },
+            _ => { first_equal = false; break; },
         }
     }
+    let mut second_equal = true;
+    for (key, value2) in &s1char_map {
+        match s1char_map.get(key) {
+            Some(value1) if value2 == value1 => {},
+            _ => { second_equal = false; break; },
+        }
+    }
+    let all_equal = if first_equal && second_equal {true} else {false};
     all_equal
 }
 
@@ -27,8 +38,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let word = "thought";
-	let word1 = "thougth";
+        let word = "cde";
+	let word1 = "edbca";
 	println!(
 		"Is `{}` a permutation of `{}`? = {}",
 		word,
