84fd3a3c2ffe29201968c78d5f0cfbba6f990eb3
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-22 17:31:42 +0000

    Final

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -2,9 +2,8 @@ use chrono::{Datelike, NaiveDate};
 pub use chrono::Weekday as wd;
 
 pub fn middle_day(year: i32) -> Option<wd> {
-    let number_of_days_in_year = if NaiveDate::from_ymd_opt(year, 1, 1).is_some() {365} else {366};
-    println!("{}", number_of_days_in_year);
-    if (number_of_days_in_year / 2) == 0 {
+    let number_of_days_in_year = if NaiveDate::from_ymd_opt(year, 2, 29).is_some() {366} else {365};
+    if number_of_days_in_year == 366 {
         None
     } else {
         Some(NaiveDate::from_ymd(year, 7, 2).weekday())
@@ -18,7 +17,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-    println!("{:?}", middle_day(1892).unwrap());
+    println!("{:?}", middle_day(1022).unwrap());
 
     }
 }
