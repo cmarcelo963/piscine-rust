303094968ba2d82bb41a81401058a7b70235e5d6
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-22 21:53:20 +0000

    try

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -7,15 +7,24 @@ pub fn expected_variable(string_to_compare: &str, expected_string: &str) -> Opti
     if string_to_compare == "" || expected_string == "" {
         return None
     }
-    if !string_to_compare.is_camel_lowercase() && !string_to_compare.contains('_'){
-        None
-    } else {
-        let distance  = edit_distance::edit_distance(string_to_compare, expected_string);
+    // !string_to_compare.is_camel_lowercase() &&
+    // !string_to_compare.contains('_')
+    // println!("String to compare: {}, expected string: {}", string_to_compare, expected_string);
+    // if !string_to_compare.is_camel_lowercase() {
+    //     println!("Its failing: {}", string_to_compare.is_camel_lowercase());
+    //     None
+    // } else 
+    // if string_to_compare.contains('_')  {
+    //     None
+    // }
+        else {
+        let distance  = edit_distance::edit_distance(&string_to_compare.to_lowercase(), &expected_string.to_lowercase()) as f32;
         println!("String to compare: {}, String expected: {}", string_to_compare, expected_string);
-        let alikeness = 100 - (distance / expected_string.len() * 100);
-        println!("{}", alikeness);
-        if alikeness > 50 {
-            Some(alikeness.to_string() + "%")
+        let expected_length = expected_string.len() as f32;
+        let alikeness = (1.0 - (distance / expected_length)) * 100.0;
+        println!("{}, {}, {}", alikeness, distance, expected_string.len());
+        if alikeness > 50.0 {
+            Some(format!("{:.2}%", alikeness.ceil()))
         } else {
             None
         }
@@ -32,17 +41,17 @@ mod tests {
         "{} close to it",
         expected_variable("On_Point", "on_point").unwrap()
     );
-    // println!(
-    //     "{} close to it",
-    //     expected_variable("soClose", "So_Close").unwrap()
-    // );
-    // println!(
-    //     "{:?}",
-    //     expected_variable("something", "something_completely_different")
-    // );
-    // println!(
-    //     "{} close to it",
-    //     expected_variable("BenedictCumberbatch", "BeneficialCucumbersnatch").unwrap()
-    // );
+    println!(
+        "{} close to it",
+        expected_variable("soClose", "So_Close").unwrap()
+    );
+    println!(
+        "{:?}",
+        expected_variable("something", "something_completely_different")
+    );
+    println!(
+        "{} close to it",
+        expected_variable("BenedictCumberbatch", "BeneficialCucumbersnatch").unwrap()
+    );
     }
 }
