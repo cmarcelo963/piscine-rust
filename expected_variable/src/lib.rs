7ec8fc9dbba54e53611eceb8bdaecb833ba6b499
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-22 20:19:55 +0000

    Try

---
+++ expected_variable/src/lib.rs
@@ -0,0 +1,54 @@
+pub use case;
+pub use case::CaseExt;
+pub mod edit_distance;
+
+
+pub fn expected_variable(string_to_compare: &str, expected_string: &str) -> Option<String> {
+    if !string_to_compare.is_camel_lowercase() || string_to_compare.contains('_'){
+        None
+    } else {
+        let distance  = edit_distance::edit_distance(string_to_compare, expected_string);
+        let alikeness = 100 - (distance / expected_string.len() * 100);
+        if alikeness > 50 {
+            Some(alikeness.to_string() + "%")
+        } else {
+            None
+        }
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        println!(
+        "{} close to it",
+        expected_variable("On_Point", "on_point").unwrap()
+    );
+    println!(
+        "{} close to it",
+        expected_variable("soClose", "So_Close").unwrap()
+    );
+    println!(
+        "{:?}",
+        expected_variable("something", "something_completely_different")
+    );
+    println!(
+        "{} close to it",
+        expected_variable("BenedictCumberbatch", "BeneficialCucumbersnatch").unwrap()
+    );
+    }
+}
