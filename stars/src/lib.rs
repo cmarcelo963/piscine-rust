db863008de1ad4fd3882295b881b12ef9f3791a1
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-23 10:53:11 +0000

    Stars

---
+++ stars/src/lib.rs
@@ -0,0 +1,29 @@
+pub fn stars(n: u32) -> String {
+    let mut counter = 1;
+    let mut number_of_stars = 2;
+    let mut result = String::new();
+    loop { 
+        if counter != n {
+            number_of_stars *= 2;
+            counter += 1;
+        } else {
+            break
+        }
+    }
+    for _ in 0..number_of_stars {
+        result += "*"
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        println!("{}", stars(1));
+        println!("{}", stars(4));
+        println!("{}", stars(5));
+    }
+}
