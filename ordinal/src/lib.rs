1a10d39761226fb41c898ccc797ab5ef00fc851d
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-23 11:28:31 +0000

    Ordinal

---
+++ ordinal/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn num_to_ordinal(x: u32) -> String {
+    match x % 100 {
+        11..=13 => format!("{}th", x),
+        _ => match x % 10 {
+            1 => format!("{}st", x),
+            2 => format!("{}nd", x),
+            3 => format!("{}rd", x),
+            _ => format!("{}th", x),
+        },
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        println!("{}", num_to_ordinal(1));
+        println!("{}", num_to_ordinal(22));
+        println!("{}", num_to_ordinal(43));
+        println!("{}", num_to_ordinal(47));
+    }
+}
