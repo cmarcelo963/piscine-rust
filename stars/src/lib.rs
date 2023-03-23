9ed5743bd0954bd1ea58e01a5f5965d7e9f6c448
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-23 11:02:10 +0000

    Stars

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,20 +1,23 @@
 pub fn stars(n: u32) -> String {
-    let mut counter = 1;
-    let mut number_of_stars = 2;
-    let mut result = String::new();
-    println!("{}", n);
-    loop { 
-        if counter != n {
-            number_of_stars *= 2;
-            counter += 1;
-        } else {
-            break
-        }
+    match u32::checked_pow(2, n) {
+        Some(pow) => "*".repeat(pow as usize),
+        None => "".to_string(),
     }
-    for _ in 0..number_of_stars {
-        result += "*"
-    }
-    result
+    // let mut counter = 1;
+    // let mut number_of_stars: u64= 2;
+    // let mut result = String::new();
+    // loop { 
+    //     if counter != n {
+    //         number_of_stars *= 2;
+    //         counter += 1;
+    //     } else {
+    //         break
+    //     }
+    // }
+    // for _ in 0..number_of_stars {
+    //     result += "*"
+    // }
+    // result
 }
 
 #[cfg(test)]
@@ -25,6 +28,6 @@ mod tests {
     fn it_works() {
         println!("{}", stars(1));
         println!("{}", stars(4));
-        println!("{}", stars(5));
+        println!("{}", stars(200));
     }
 }
