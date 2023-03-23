ef08c42f428ac59959640a0b86e19c4c288bfebd
cmarcelo963 <ianmarcelo963@gmail.com>
2023-03-23 14:10:30 +0000

    Talk

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,8 +1,20 @@
 pub fn talking(text: &str) -> &str {
     println!("{}", text);
-    if is_all_caps(text) && text.chars().last() == Some('!') {
+    let filtered_input = text.chars().filter(|c| c.is_alphabetic());
+    let mut is_all_caps = true;
+
+     for c in filtered_input {
+        // Check if the character is an uppercase letter
+        if !c.is_uppercase() {
+            // If not, return false
+            is_all_caps = false;
+            break;
+        }
+    }
+
+    if is_all_caps && text.chars().last() == Some('!') {
         return "There is no need to yell, calm down!"
-    } else if is_all_caps(text) && text.chars().last() == Some('?') {
+    } else if is_all_caps && text.chars().last() == Some('?') {
         return "Quiet, I am thinking!"
     } else if text.chars().last() == Some('?') {
         return "Sure."
@@ -11,21 +23,7 @@ pub fn talking(text: &str) -> &str {
     }
     return "Interesting"
 }
-pub fn is_all_caps(input: &str) -> bool {
-    // Filter out non-alphabetic characters
-    let filtered_input = input.chars().filter(|c| c.is_alphabetic());
 
-    // Iterate over each character in the filtered string
-    for c in filtered_input {
-        // Check if the character is an uppercase letter
-        if !c.is_uppercase() {
-            // If not, return false
-            return false;
-        }
-    }
-    // If all characters are uppercase, return true
-    true
-}
 
 #[cfg(test)]
 mod tests {
@@ -37,6 +35,6 @@ mod tests {
         println!("{:?}", talking("Hello how are you?"));
         println!("{:?}", talking("WHAT'S GOING ON?"));
         println!("{:?}", talking("something"));
-        println!("{:?}", talking(""));
+        println!("{:?}", talking("I LOVE YELLING"));
     }
 }
